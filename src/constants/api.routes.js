3398fd321bfa48e1a5c48ae658866f6d90e0060e
Devxxxcode <devxxxcode12345@gmail.com>
2026-05-27 02:05:00 -0700

    code

--- src/constants/api.routes.js
+++ src/constants/api.routes.js
@@ -3,7 +3,10 @@
 export const BASEURL = "https://mososoup-api-1.onrender.com";
 
 // export const BASEURL = "http://127.0.0.1:8000";
-h
+export const loginAPI = `${BASEURL}/auth/login/`;
+export const registerAPI = `${BASEURL}/auth/signup/`;
+export const refreshToken = `${BASEURL}/auth/refresh-token/`;
+export const meAPI = `${BASEURL}/auth/me/`;
 export const updateAPI = `${BASEURL}/auth/update_profile/`;
 export const passwordAPI = `${BASEURL}/auth/user_change_password/`;
 export const transactionPasswordAPI = `${BASEURL}/auth/user_change_transactional_password/`;
